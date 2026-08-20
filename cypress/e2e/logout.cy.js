ebd0b5928e8bb4fde670d412be64a896a24a6a7b
Mia N <miamia11204@gmail.com>
2026-08-20 16:10:20 -0500

    test: add logout test case

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,17 @@
+import loginPage from '../pages/LoginPage';
+
+describe('SauceDemo Logout Automation', () => {
+  beforeEach(() => {
+    loginPage.visitPage();
+    loginPage.login('standard_user', 'secret_sauce');
+  });
+
+  it('should logout the user successfully', () => {
+    // Click on the menu button to open the sidebar
+    cy.get('#react-burger-menu-btn').click();
+    // Click on the logout link in the sidebar
+    cy.get('#logout_sidebar_link').click();
+    // Verify that the user is redirected to the login page after logout
+    cy.url().should('include', '/');
+  });
+});
